fb8348a1c6c72200fb637b09091ff37ae0633355
elmurd0r <gameralc@web.de>
2021-08-27 10:04:31 +0200

    Add custom bpmn replace, popupmenu, replacemenuprovider and replaceoptions to replace iot artifacts. Add missing dependency to package.json. Add custom modules to bpmn modeler.

--- app/custom/index.js
+++ app/custom/index.js
@@ -2,11 +2,17 @@ import CustomContextPad from './CustomContextPad';
 import CustomPalette from './CustomPalette';
 import CustomRenderer from './CustomRenderer';
 import ResizeTask from "./ResizeTask";
+import CustomBpmnReplace from "./replace/CustomBpmnReplace";
+import CustomReplaceMenuProvider from "./replace/CustomReplaceMenuProvider";
+import CustomPopupMenu from "./replace/CustomPopupMenu";
 
 export default {
-  __init__: [ 'customContextPad', 'customPalette', 'customRenderer', 'resizeTask'],
+  __init__: [ 'customContextPad', 'customPalette', 'customRenderer', 'resizeTask', 'replaceMenuProvider', 'bpmnReplace', 'popupMenu'],
   customContextPad: [ 'type', CustomContextPad ],
   customPalette: [ 'type', CustomPalette ],
   customRenderer: [ 'type', CustomRenderer ],
-  resizeTask: [ 'type', ResizeTask]
+  resizeTask: [ 'type', ResizeTask],
+  replaceMenuProvider: ['type', CustomReplaceMenuProvider],
+  bpmnReplace: ['type', CustomBpmnReplace],
+  popupMenu: ['type', CustomPopupMenu]
 };
