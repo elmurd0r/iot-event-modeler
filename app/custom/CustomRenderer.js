4ab261eb96012e90696fc04c14944df29602e374
Florian Gallik <florian-1.gallik@uni-ulm.de>
2022-04-26 10:18:42 +0200

    Add function to render path. Use path function for decision group.

--- app/custom/CustomRenderer.js
+++ app/custom/CustomRenderer.js
@@ -1,6 +1,5 @@
 import BaseRenderer from 'diagram-js/lib/draw/BaseRenderer';
-
-import {getFillColor} from "bpmn-js/lib/draw/BpmnRenderUtil";
+import {getFillColor, getStrokeColor} from "bpmn-js/lib/draw/BpmnRenderUtil";
 
 
 import {
@@ -68,6 +67,22 @@ export default class CustomRenderer extends BaseRenderer {
       svgAppend(parentNode, img);
       return img;
     }
+    if (!isNil(iotType) && iotType === 'decision-group') {
+      const DECISION_CONTAINER_PATH = {
+        d: 'm {mx},{my} l 0 12 c 0 0 5 0 5 -6 c 0 -1 0 -6 -5 -6 z'
+      }
+      const abspos = {
+        abspos: {
+          x: 8,
+          y:8
+        }
+      }
+      let pathData = getScaledPath(DECISION_CONTAINER_PATH, abspos)
+      this.bpmnRenderer._drawPath(parentNode, pathData, {
+        strokeWidth: 2,
+        stroke: getStrokeColor(element, 'black')
+      });
+    }
 
     return shape;
   }
@@ -110,4 +125,74 @@ function drawImg(width, height, image, color) {
   }
 
   return img;
+}
+
+function getScaledPath(rawPath, param) {
+  // positioning
+  // compute the start point of the path
+  var mx, my;
+
+  if (param.abspos) {
+    mx = param.abspos.x;
+    my = param.abspos.y;
+  } else {
+    mx = param.containerWidth * param.position.mx;
+    my = param.containerHeight * param.position.my;
+  }
+
+  var coordinates = {}; // map for the scaled coordinates
+  if (param.position) {
+
+    // path
+    var heightRatio = (param.containerHeight / rawPath.height) * param.yScaleFactor;
+    var widthRatio = (param.containerWidth / rawPath.width) * param.xScaleFactor;
+
+
+    // Apply height ratio
+    for (var heightIndex = 0; heightIndex < rawPath.heightElements.length; heightIndex++) {
+      coordinates['y' + heightIndex] = rawPath.heightElements[heightIndex] * heightRatio;
+    }
+
+    // Apply width ratio
+    for (var widthIndex = 0; widthIndex < rawPath.widthElements.length; widthIndex++) {
+      coordinates['x' + widthIndex] = rawPath.widthElements[widthIndex] * widthRatio;
+    }
+  }
+
+  // Apply value to raw path
+  var path = format(
+      rawPath.d, {
+        mx: mx,
+        my: my,
+        e: coordinates
+      }
+  );
+  return path;
+};
+
+
+// copied and adjusted from https://github.com/adobe-webplatform/Snap.svg/blob/master/src/svg.js
+var tokenRegex = /\{([^{}]+)\}/g,
+    objNotationRegex = /(?:(?:^|\.)(.+?)(?=\[|\.|$|\()|\[('|")(.+?)\2\])(\(\))?/g; // matches .xxxxx or ["xxxxx"] to run over object properties
+
+function replacer(all, key, obj) {
+  var res = obj;
+  key.replace(objNotationRegex, function(all, name, quote, quotedName, isFunc) {
+    name = name || quotedName;
+    if (res) {
+      if (name in res) {
+        res = res[name];
+      }
+      typeof res == 'function' && isFunc && (res = res());
+    }
+  });
+  res = (res == null || res == obj ? all : res) + '';
+
+  return res;
+}
+
+function format(str, obj) {
+  return String(str).replace(tokenRegex, function(all, key) {
+    return replacer(all, key, obj);
+  });
 }
