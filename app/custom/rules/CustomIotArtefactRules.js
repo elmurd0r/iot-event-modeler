2d82d67de20f3d1357ba2f097255864542f3f362
Florian Gallik <florian-1.gallik@uni-ulm.de>
2022-11-24 18:30:42 +0100

    Change rules to create sequence flow when creating connections to/from decision elements. Change rules to add decision elements to the container. Add attach rule to attach iot conditional intermediate event to a task and act as a boundary event.

--- app/custom/rules/CustomIotArtefactRules.js
+++ app/custom/rules/CustomIotArtefactRules.js
@@ -38,8 +38,24 @@ CustomIotArtefactRules.prototype.init = function() {
     }
     // priority is important
     this.addRule('connection.create', 1500, (context) => {
+
+        if ((context.source.businessObject.gateway === 'result' || context.source.businessObject.type === 'decision-group') && context.target.businessObject.gateway === 'condition' ) {
+            return {
+                type: 'bpmn:SequenceFlow'
+            };
+        }
+        if (context.source.businessObject.type === 'sensor' && context.target.businessObject.gateway === 'condition' ) {
+            return {
+                type: 'bpmn:SequenceFlow'
+            };
+        }
+        if ((context.source.type === 'bpmn:DataObjectReference' && !context.source.businessObject.type)) {
+            return {
+                type: 'bpmn:SequenceFlow'
+            };
+        }
         if(context.source.businessObject.type === 'decision-group' || context.target.businessObject.type === 'decision-group') {
-            if((context.target.type === 'bpmn:IntermediateCatchEvent' || context.target.type === 'bpmn:StartEvent' || context.target.type === 'bpmn:BoundaryEvent') && context.target.businessObject.eventDefinitions[0]['$type'] === 'bpmn:ConditionalEventDefinition') {
+            if((context.target.type === 'bpmn:IntermediateCatchEvent' || context.target.type === 'bpmn:StartEvent' || context.target.type === 'bpmn:BoundaryEvent') && context.target.businessObject.eventType) {
                 return {
                     type: 'bpmn:Association'
                 };
@@ -53,20 +69,27 @@ CustomIotArtefactRules.prototype.init = function() {
     this.addRule('shape.create', 1500, (context) => {
         if(context.target.businessObject.type === 'decision-group') {
             let shapeBoType = context.shape.businessObject.type;
-            let creatable = shapeBoType === 'sensor' || shapeBoType === 'decision-group' || context.shape.businessObject.operator || (context.shape.type === 'bpmn:DataObjectReference' && !shapeBoType)
+            let shapeBoGateway = context.shape.businessObject.gateway;
+            let creatable = shapeBoType === 'sensor' || shapeBoType === 'decision-group' || shapeBoGateway || (context.shape.type === 'bpmn:DataObjectReference' && !shapeBoType)
             return creatable;
         }
     });
 
     this.addRule('elements.move', 1500, (context) => {
         if ( context.target?.businessObject.type === 'decision-group') {
-            let movable =  context.shapes.every(shape => shape.businessObject.type === 'sensor' || shape.businessObject.type === 'decision-group' || shape.businessObject.operator || (shape.type === 'bpmn:DataObjectReference' && !shape.businessObject.type));
+            let movable =  context.shapes.every(shape => shape.businessObject.type === 'sensor' || shape.businessObject.type === 'decision-group' || shape.businessObject.gateway || (shape.type === 'bpmn:DataObjectReference' && !shape.businessObject.type));
             return movable;
         }
     });
 
     this.addRule('connection.reconnect', 1500, (context) => {
         return handleAssociation(context);
+    });
+
+    this.addRule('shape.attach', 1500, function(context) {
+        if(context.target.type === 'bpmn:Task' && context.shape.businessObject.eventType === 'cond-interm') {
+            return true;
+        }
     });
 };
 
